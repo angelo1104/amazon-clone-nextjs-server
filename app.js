86835d05a5bfeb316b012dfa4f59801030bea7ef
Madhav Anand <madhav1004@icloud.com>
2021-01-28 19:21:00 +0530

    updated packages.json packages

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
-import dotenv from 'dotenv';
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 //imports
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser'
-import authRouter from "./Routes/auth.js";
-import stripeRouter from './Routes/stripe.js';
-import productsRouter from './Routes/products.js';
-import db from './mongoDB/connection.js'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import stripeRouter from "./Routes/stripe.js";
+import productsRouter from "./Routes/products.js";
+import db from "./mongoDB/connection.js";
 import ordersRouter from "./Routes/orders.js";
 
 //app define
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 const app = express();
 
 //database realtime stuff
-mongoose.connect(process.env.MONGOOSE_URL, {useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify: false},(error)=>{
-    console.log('Error in MongoDB --->',error)
-})
+mongoose.connect(
+  process.env.MONGOOSE_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
+  (error) => {
+    console.log("Error in MongoDB --->", error);
+  }
+);
 
-db.once('open',()=>{
-    console.log('MongoDB connected to the remote db.')
-})
+db.once("open", () => {
+  console.log("MongoDB connected to the remote db.");
+});
 
 //middlewares
 app.use(cors());
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use('/auth', authRouter)
+app.use("/payments", stripeRouter);
 
-app.use('/payments', stripeRouter)
+app.use("/products", productsRouter);
 
-app.use('/products', productsRouter)
-
-app.use('/orders', ordersRouter)
+app.use("/orders", ordersRouter);
 
 //api routes
-app.get('/',(req, res) => {
-    res.send('Hello is this amazon you are in the correct palace.');
-})
-
-//listen command
-app.listen(port,()=>{
-    console.log('Server started on port ' + port);
+app.get("/", (req, res) => {
+  res.send("Hello is this amazon you are in the correct palace.");
 });
 
+//listen command
+app.listen(port, () => {
+  console.log("Server started on port " + port);
+});
